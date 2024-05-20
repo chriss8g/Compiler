6c610cb3472261dfceb49c871cabd738f7bc59c8
anuno61044 <anuno61044@gmail.com>
2024-05-20 10:10:45 -0400

    add bool operators

--- lexer.py
+++ lexer.py
@@ -9,6 +9,16 @@ tokens = (
     'DIVIDE',
     'LPAREN',
     'RPAREN',
+    'AND',
+    'OR', 
+    'NOT', 
+    'BOOL',
+    'LT',  # Menor que
+    'GT',  # Mayor que
+    'LE',  # Menor o igual
+    'GE',  # Mayor o igual
+    'EQ',  # Igual
+    'NEQ',  # No igual
 )
 
 # Expresiones regulares para tokens simples
@@ -18,6 +28,16 @@ t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_AND = r'\bAND\b'
+t_OR = r'\bOR\b'
+t_NOT = r'\bNOT\b'
+t_BOOL = r'(?:\bTRUE\b)|(?:\bFALSE\b)'
+t_LT = r'<'
+t_GT = r'>'
+t_LE = r'<='
+t_GE = r'>='
+t_EQ = r'=='
+t_NEQ = r'!='
 
 # Definición de un número (secuencia de dígitos)
 def t_NUMBER(t):
@@ -38,7 +58,7 @@ lexer = lex.lex()
 
 # Prueba del lexer
 if __name__ == "__main__":
-    data = "3 + 5 * ( 10 - 20 ) / 2"
+    data = "(4+  3 ) >= 4"
     lexer.input(data)
     while True:
         tok = lexer.token()
