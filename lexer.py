f71d37805e112de842a602b2f055096d64c1cb33
Amanda Cordero <amandacordero.1709@gmail.com>
2024-05-21 19:42:54 -0400

    ParserForString

--- lexer.py
+++ lexer.py
@@ -9,6 +9,8 @@ tokens = (
     'DIVIDE',
     'LPAREN',
     'RPAREN',
+    'CONCAT',
+    'STRING',
 )
 
 # Expresiones regulares para tokens simples
@@ -18,12 +20,20 @@ t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_CONCAT = r'@'
 
 # Definición de un número (secuencia de dígitos)
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
+
+# Definicion de una cadena
+def t_STRING(t):
+    r'"[^"]*"'  # Expresión regular que coincide con una secuencia de caracteres entre comillas dobles.
+    t.value = t.value[1:-1]  # ???Elimina las comillas dobles del inicio y final de la cadena.
+    # print(t)
+    return t  # Devuelve el token procesado.
 
 # Ignorar espacios en blanco
 t_ignore = ' \t'
@@ -38,7 +48,8 @@ lexer = lex.lex()
 
 # Prueba del lexer
 if __name__ == "__main__":
-    data = "3 + 5 * ( 10 - 20 ) / 2"
+    #data = "3 + 5 * ( 10 - 20 ) / 2"
+    data = "\"unstrin\" @ \"otrostrin\""
     lexer.input(data)
     while True:
         tok = lexer.token()
