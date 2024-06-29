5a793d5f16c20095bb606a2912536fad0d4bf683
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-06-29 19:11:28 -0400

    reduce to class

---
+++ grammer.py
@@ -0,0 +1,123 @@
+from cmp.pycompiler import Grammar
+from my_types import *
+
+class CodeToAST:
+    
+    def __init__(self, text):
+        self.G = Grammar()
+
+        # Definir los no terminales
+        program = self.G.NonTerminal('<program>', startSymbol=True)
+        stat_list, stat = self.G.NonTerminals('<stat_list> <stat>')
+        expr, term, factor, atom = self.G.NonTerminals('<expr> <term> <factor> <atom>')
+        arg_list, func_call, expr_list, asig_list = self.G.NonTerminals('<arg_list> <func_call> <expr_list> <asig_list>')
+
+        # Definir los terminales
+        let, functionx, inx = self.G.Terminals('let function in')
+        printx, sin, cos, sqrt, exp, log, rand = self.G.Terminals('print sin cos sqrt exp log rand')
+        semi, comma, opar, cpar, arrow = self.G.Terminals('; , ( ) =>')
+        asign, plus, minus, star, div = self.G.Terminals('= + - * /')
+        pow, mod, andx, orx, notx = self.G.Terminals('^ % & | !')
+        eq, gt, lt, ge, le = self.G.Terminals('== > < >= <=')
+        ne, concat, lbrace, rbrace, asign2 = self.G.Terminals('!= @ { } :=')
+        pi, e, true, false = self.G.Terminals('PI E true false')
+        idx, number, string = self.G.Terminals('id num string')
+        ifx, elsex, elifx, whilex, forx, range = self.G.Terminals('if else elif while for range')
+
+        # Definir las producciones y sus acciones
+        program %= stat_list, lambda h, s: ProgramNode(s[1])
+
+        stat_list %= stat + semi, lambda h, s: [s[1]]
+        stat_list %= stat + semi + stat_list, lambda h, s: [s[1]] + s[3]
+
+        stat %= let + asig_list + inx + expr, lambda h, s: VarDeclarationNode(s[2][0], s[2][1], s[4])
+        stat %= functionx + idx + opar + arg_list + cpar + arrow + expr, lambda h, s: FuncDeclarationNode(s[2], s[4], s[7])
+        stat %= printx + expr, lambda h, s: PrintNode(s[2])
+        stat %= whilex + opar + expr + cpar + stat, lambda h, s: WhileNode(s[3], s[5])
+        stat %= forx + opar + idx + inx + expr + cpar + stat, lambda h, s: ForNode(s[3], s[5], s[7])
+        stat %= ifx + opar + expr + cpar + stat + elsex + stat, lambda h, s: IfNode(s[3], s[5], s[7], [], [])
+        stat %= ifx + opar + expr + cpar + stat + elifx + opar + expr + cpar + stat + elsex + stat, lambda h, s: IfNode(s[3], s[5], s[11], [s[8]], [s[10]])
+
+        arg_list %= idx, lambda h, s: [s[1]]
+        arg_list %= idx + comma + arg_list, lambda h, s: [s[1]] + s[3]
+
+        expr %= expr + plus + term, lambda h, s: PlusNode(s[1], s[3])
+        expr %= expr + minus + term, lambda h, s: MinusNode(s[1], s[3])
+        expr %= term, lambda h, s: s[1]
+
+        term %= term + star + factor, lambda h, s: StarNode(s[1], s[3])
+        term %= term + div + factor, lambda h, s: DivNode(s[1], s[3])
+        term %= factor, lambda h, s: s[1]
+
+        factor %= opar + expr + cpar, lambda h, s: s[2]
+        factor %= number, lambda h, s: ConstantNumNode(s[1])
+        factor %= true, lambda h, s: BoolNode(s[1])
+        factor %= false, lambda h, s: BoolNode(s[1])
+        factor %= pi, lambda h, s: ConstantNumNode(s[1])
+        factor %= e, lambda h, s: ConstantNumNode(s[1])
+        factor %= string, lambda h, s: StringNode(s[1])
+        factor %= idx, lambda h, s: VariableNode(s[1])
+        factor %= func_call, lambda h, s: s[1]
+
+        func_call %= idx + opar + expr_list + cpar, lambda h, s: CallNode(s[1], s[3])
+
+        expr_list %= expr, lambda h, s: [s[1]]
+        expr_list %= expr + comma + expr_list, lambda h, s: [s[1]] + s[3]
+
+        asig_list %= idx + asign + expr, lambda h, s: [[s[1]], [s[3]]]
+        asig_list %= idx + asign + expr + comma + asig_list, lambda h, s: [[s[1]] + s[5][0], [s[3]] + s[5][1]]
+
+        #############################################################################
+
+
+        from cmp.utils import Token, tokenizer
+
+        fixed_tokens = { t.Name: Token(t.Name, t) for t in self.G.terminals if t not in { idx, number }}
+
+        @tokenizer(self.G, fixed_tokens)
+        def tokenize_text(token):
+            lex = token.lex
+            try:
+                float(lex)
+                return token.transform_to(number)
+            except ValueError:
+                    return token.transform_to(idx)
+
+        tokens = tokenize_text(text)
+
+
+        ###################################################################################
+
+
+        from cmp.tools.parsing import LR1Parser
+
+        parser = LR1Parser(self.G)
+
+
+        parse, operations = parser([t.token_type for t in tokens], get_shift_reduce=True)
+        # print('\n'.join(repr(x) for x in parse))
+
+
+        from cmp.evaluation import evaluate_reverse_parse
+
+        self.ast = evaluate_reverse_parse(parse, operations, tokens)
+        # print(ast)
+
+    def __repr__(self):
+        from FormatVisitor import FormatVisitor
+
+        formatter = FormatVisitor()
+        tree = formatter.visit(self.ast)
+        return tree
+    
+
+if __name__ == "__main__":
+      
+      text ='''
+                print 1 - 1 - 1 ;
+                let x = 58 in x + 1 ;
+                function f ( a , b ) => 5 + 6 ;
+                print F ( 5 + x , 7 + y ) ;
+            '''
+      codeToAST = CodeToAST(text)
+      print(codeToAST)
--- my_types.py
+++ my_types.py
@@ -4,3 +4,219 @@ BOOL_TYPE = 'bool'
 STRING_TYPE = 'string'
 CONST_TYPE = 'const'
 NUMBER_TYPE = [INT_TYPE, FLOAT_TYPE, CONST_TYPE]
+
+# Nivel 0
+
+
+class Node:
+    pass
+
+# Nivel 1
+
+
+class ProgramNode(Node):
+    def __init__(self, statements):
+        self.statements = statements
+
+
+class StatementNode(Node):
+    pass
+
+
+class ExpressionNode(Node):
+    pass
+
+
+# Nivel 2
+
+class VarDeclarationNode(StatementNode, ExpressionNode):
+    def __init__(self, idxs, exprs, body):
+        self.ids = idxs
+        self.exprs = exprs
+        self.body = body
+
+
+class FuncDeclarationNode(StatementNode, ExpressionNode):
+    def __init__(self, idx, params, body):
+        self.id = idx
+        self.params = params
+        self.body = body
+
+
+class PrintNode(ExpressionNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class SinNode(ExpressionNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class CosNode(ExpressionNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class SqrtNode(ExpressionNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class ExpNode(ExpressionNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class LogNode(ExpressionNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class RandNode(ExpressionNode):
+    pass
+
+
+class ForNode(StatementNode, ExpressionNode):
+    def __init__(self, idx, iterable, expr):
+        self.id = idx
+        self.iterable = iterable
+        self.expr = expr
+
+
+class WhileNode(StatementNode, ExpressionNode):
+    def __init__(self, condition, expr):
+        self.condition = condition
+        self.expr = expr
+
+
+class IfNode(StatementNode, ExpressionNode):
+    def __init__(self, condition, expr, else_expr, elif_conditions, elif_expr):
+        self.condition = condition
+        self.expr = expr
+        self.else_expr = else_expr
+        self.elif_conditions = elif_conditions
+        self.elif_expr = elif_expr
+
+
+class IterableNode(ExpressionNode):
+    def __init__(self, init, final):
+        self.init = init
+        self.final = final
+
+
+class BlockNode(StatementNode):
+    def __init__(self, body):
+        self.body = body
+
+
+class AtomicNode(ExpressionNode):
+    def __init__(self, lex):
+        self.lex = lex
+
+
+class BinaryNode(ExpressionNode):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class CallNode(ExpressionNode):
+    def __init__(self, idx, args):
+        self.id = idx
+        self.args = args
+
+
+# Nivel 3
+
+class ConstantNumNode(AtomicNode):
+    pass
+
+
+class BoolNode(AtomicNode):
+    pass
+
+
+class StringNode(AtomicNode):
+    pass
+
+
+class VariableNode(AtomicNode):
+    pass
+
+
+class ComparativeNode(BinaryNode):
+    pass
+
+
+class LogicNode(BinaryNode, AtomicNode):
+    pass
+
+
+class AritmeticNode(BinaryNode):
+    pass
+
+
+class StringOpNode(BinaryNode):
+    pass
+
+
+# Nivel 4
+class PlusNode(AritmeticNode):
+    pass
+
+
+class MinusNode(AritmeticNode):
+    pass
+
+
+class StarNode(AritmeticNode):
+    pass
+
+
+class DivNode(AritmeticNode):
+    pass
+
+
+class PowNode(AritmeticNode):
+    pass
+
+
+class ModNode(AritmeticNode):
+    pass
+
+
+class EQNode(ComparativeNode):
+    pass
+
+
+class GTNode(ComparativeNode):
+    pass
+
+
+class LTNode(ComparativeNode):
+    pass
+
+
+class GENode(ComparativeNode):
+    pass
+
+
+class LENode(ComparativeNode):
+    pass
+
+
+class NENode(ComparativeNode):
+    pass
+
+
+class AndNode(LogicNode):
+    pass
+
+
+class OrNode(LogicNode):
+    pass
+
+
+class ConcatNode(StringOpNode):
+    pass
