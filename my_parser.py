bfa3d25247ad82074ec623fff1ca8bd5b3e64ee4
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-07-09 11:56:17 -0400

    mend

--- my_parser.py
+++ my_parser.py
@@ -237,7 +237,6 @@ class CodeToAST:
 
         # ***************** Expresiones ******************
         expr %= blockExpr, lambda h, s: s[1]
-        expr %= let + asig_list + inx + expr, lambda h, s: LetNode(s[2], s[4])
         expr %= let + asig_list + inx + expr, lambda h, s: MultipleLet(s)
         expr %= ifx + opar + expr + cpar + specialBlock + elifx_expr + elsex + superexpr, lambda h, s: IfNode(s[3], s[5], s[8], s[6][0], s[6][1])
         expr %= whilex + opar + expr + cpar + expr, lambda h, s: WhileNode(s[3], s[5])
