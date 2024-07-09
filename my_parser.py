f562454e4a38c4abf1c9ec6659ebba50ae256540
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-07-09 11:34:47 -0400

    mend

--- my_parser.py
+++ my_parser.py
@@ -237,16 +237,16 @@ class CodeToAST:
 
         # ***************** Expresiones ******************
         expr %= blockExpr, lambda h, s: s[1]
-        # expr %= let + asig_list + inx + expr, lambda h, s: LetNode(s[2], s[4])
+        expr %= let + asig_list + inx + expr, lambda h, s: LetNode(s[2], s[4])
         expr %= let + asig_list + inx + expr, lambda h, s: MultipleLet(s)
         expr %= ifx + opar + expr + cpar + specialBlock + elifx_expr + elsex + superexpr, lambda h, s: IfNode(s[3], s[5], s[8], s[6][0], s[6][1])
         expr %= whilex + opar + expr + cpar + expr, lambda h, s: WhileNode(s[3], s[5])
-        # expr %= forx + opar + idnode + inx + rangex + opar + expr + comma + expr + cpar + cpar + expr, lambda h, s: ForRangeToWhile(s)
-        # expr %= forx + opar + idnode + inx + idnode + cpar + expr, lambda h, s: ForToWhile(s)
+        expr %= forx + opar + idnode + inx + rangex + opar + expr + comma + expr + cpar + cpar + expr, lambda h, s: ForRangeToWhile(s)
+        expr %= forx + opar + idnode + inx + idnode + cpar + expr, lambda h, s: ForToWhile(s)
         expr %= printx + opar + expr + cpar, lambda h, s: PrintNode(s[3])
         expr %= idnode + asign2 + expr, lambda h, s: DestructNode(s[1], s[3])
-        # expr %= new + idx + opar + arg_expr + cpar, lambda h, s: ObjectCreationNode(s[2], s[4])
-        # expr %= new + idx + opar + cpar, lambda h, s: ObjectCreationNode(s[2], [])
+        expr %= new + idx + opar + arg_expr + cpar, lambda h, s: ObjectCreationNode(s[2], s[4])
+        expr %= new + idx + opar + cpar, lambda h, s: ObjectCreationNode(s[2], [])
         expr %= subexpr, lambda h, s: s[1]
 
         superexpr %= expr, lambda h, s: s[1]
@@ -277,8 +277,8 @@ class CodeToAST:
 
         term %= term + star + factor, lambda h, s: StarNode(s[1], s[3])
         term %= term + div + factor, lambda h, s: DivNode(s[1], s[3])
-        # term %= term + powx + factor, lambda h, s: PowNode(s[1], s[3])
-        # term %= term + mod + factor, lambda h, s: ModNode(s[1], s[3])
+        term %= term + powx + factor, lambda h, s: PowNode(s[1], s[3])
+        term %= term + mod + factor, lambda h, s: ModNode(s[1], s[3])
         term %= factor, lambda h, s: s[1]
 
         factor %= sin + opar + expr + cpar, lambda h, s: SinNode(s[3])
@@ -296,9 +296,9 @@ class CodeToAST:
         atom %= e, lambda h, s: NumberNode(s[1])
         atom %= string, lambda h, s: StringNode(s[1])
         atom %= opar + expr + cpar, lambda h, s: s[2]
-        # atom %= selfx + dot + idnode, lambda h, s: SelfNode(s[3])
-        # atom %= selfx + dot + idx + dot + recurrent_object, lambda h, s: SelfNode(IdentifierNode(s[3],s[5]))
-        # atom %= selfx + dot + recurrent_object, lambda h, s: SelfNode(s[3])
+        atom %= selfx + dot + idnode, lambda h, s: SelfNode(s[3])
+        atom %= selfx + dot + idx + dot + recurrent_object, lambda h, s: SelfNode(IdentifierNode(s[3],s[5]))
+        atom %= selfx + dot + recurrent_object, lambda h, s: SelfNode(s[3])
         atom %= obrake + arg_expr + cbrake, lambda h, s: VectorNode(s[2])
         atom %= idnode, lambda h, s: s[1]
         atom %= idx + dot + recurrent_object, lambda h, s: IdentifierNode(s[1], s[3])
